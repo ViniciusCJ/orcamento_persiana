2cb4fca2364cf778610f05bf16ae02efad36c090
ViniciusCJ <viniciusjunqueira0809@gmail.com>
2022-08-24 12:18:08 -0300

    calcularOrcament

--- index.js
+++ index.js
@@ -1,49 +1,58 @@
-// função para calcular os valores das persianas
+
+
 function calcularOrcamento(){
+    // função para calcular os valores das persianas
     const largura = document.getElementById("larguraPersiana").value;
     const altura = document.getElementById("alturaPersiana").value;
-    const quantidade = document.getElementById("quantidadePersiana").value;
+    const qtdPersiana = document.getElementById("quantidadePersiana").value;
+    console.log("Chegou")
 
+    console.log(largura,altura,qtdPersiana)
     //valor de persiana imaginario, depois pegar valor nos dados passados no JSon
-    const valorPersiana = 129.90
+    /*const valorPersiana = 129.90
     const metroQuadrado = largura * altura
 
     const precoPersiana = (metroQuadrado * valorPersiana) * quantidade
-    console.log(precoPersiana)
+    console.log(precoPersiana)*/
 }
+
+//declarando variavel quantidade, para mostrar no titulo e nos ID
 var quantidade = 1
+//função responsavel para duplicar o form "Orçamento Persiana"
 function adicionarPersiana() {
     quantidade ++
 
-    let element = document.getElementById ("persianaContainer").innerHTML +=(`        <div class="container p-3 mb-2 bg-light text-dark rounded-3">
-    <form class="row gx-3 gy-2 align-items-center">
-        <h3>Persiana ${quantidade}</h3>
-        <div class="row g-3">
-            <div class="col-sm-6">
-                <label class="form-label" for="largura">Informe a largura:</label>
-                <input type="Number" class="form-control" placeholder="Largura" id="larguraPersiana${quantidade}" aria-label="larguraPersiana">
+    let element = document.getElementById ("persianaContainer").innerHTML +=(`        
+    <div class="container p-3 mb-2 bg-light text-dark rounded-3" id="card${quantidade}">
+        <form class="row gx-3 gy-2 align-items-center">
+            <h3>Persiana ${quantidade}</h3>
+            <div class="row g-3">
+                <div class="col-sm-6">
+                    <label class="form-label" for="largura">Informe a largura:</label>
+                    <input type="Number" class="form-control" placeholder="Largura" id="larguraPersiana${quantidade}" aria-label="larguraPersiana">
+                </div>
+                <div class="col-sm-6">
+                    <label class="form-label" for="altura">Informe a altura:</label>
+                    <input type="Number" class="form-control" placeholder="Altura" id ="alturaPersiana${quantidade}" aria-label="alturaPersiana">
+                </div>
+                <div class="col-sm-4">
+                    <label class="form-label" for="modelo">Informe o modelo da Persiana:</label>
+                    <input type="text" class="form-control" placeholder="Modelo" id ="modeloPersiana${quantidade}" aria-label="modeloPersiana">
+                </div>
+                <div class="col-sm-4">
+                    <label class="form-label" for="quantidade">Quantidade:</label>
+                    <input type="Number" class="form-control" placeholder="Quantidade" id ="quantidadePersiana${quantidade}" aria-label="quantidadePersiana">
+                </div>
+                <div class="col-sm-4">
+                    <label class="form-label" for="bando">Tem bandô:</label>
+                    <select class="form-select" aria-label="bando">
+                        <option selected>Tem bandô?</option>
+                        <option value="1">Sim</option>
+                        <option value="2">Não</option>
+                    </select>
+                </div>
             </div>
-            <div class="col-sm-6">
-                <label class="form-label" for="altura">Informe a altura:</label>
-                <input type="Number" class="form-control" placeholder="Altura" id ="alturaPersiana${quantidade}" aria-label="alturaPersiana">
-            </div>
-            <div class="col-sm-4">
-                <label class="form-label" for="modelo">Informe o modelo da Persiana:</label>
-                <input type="text" class="form-control" placeholder="Modelo" id ="modeloPersiana${quantidade}" aria-label="modeloPersiana">
-            </div>
-            <div class="col-sm-4">
-                <label class="form-label" for="quantidade">Quantidade:</label>
-                <input type="Number" class="form-control" placeholder="Quantidade" id ="quantidadePersiana${quantidade}" aria-label="quantidadePersiana">
-            </div>
-            <div class="col-sm-4">
-                <label class="form-label" for="bando">Tem bandô:</label>
-                <select class="form-select" aria-label="bando">
-                    <option selected>Tem bandô?</option>
-                    <option value="1">Sim</option>
-                    <option value="2">Não</option>
-                </select>
-            </div>
-        </div>
-    </form>
-</div>`)
+        </form>
+    </div>`)
+   //calcularOrcamento()
 }
